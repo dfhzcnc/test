404b532c555838d04a1afe2e8951c536f8af22bd
Lidongfang Guo <lidongfangguo@lidongfangs-mac-mini.local>
2016-06-10 13:35:25 -0500

    Initial Commit

---
+++ SoundDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundDemo
+//
+//  Created by Lidongfang Guo on 6/10/16.
+//  Copyright © 2016 Lidongfang Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
